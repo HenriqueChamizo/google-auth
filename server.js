6e64a81e6ed181bf28f462b183082e5c38959c46
Henrique Chamizo <henrique@chamizo.com>
2025-01-31 00:45:08 -0300

    add log

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+console.log("Conectando a base: ", process.env.MONGO_URI)
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
